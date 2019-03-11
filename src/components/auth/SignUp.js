c0bbdf6a294aaa70b315578024649f352d2f7c18
yashasvi13 <yashasvij13@gmail.com>
2019-03-11 15:38:39 +0530

    added css

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -41,7 +41,7 @@ class SignUp extends Component {
           </div>
 
           <div className="input-field">
-            <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+            <button className="btn brown z-depth-0">Sign Up</button>
           </div>
         </form>
       </div>
